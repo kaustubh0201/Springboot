56033f56b131770d309c7756630d4c97eda39c56
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-10-14 23:49:50 +0530

    added a button to add new customer

--- web-customer-tracker/WebContent/WEB-INF/view/list-customers.jsp
+++ web-customer-tracker/WebContent/WEB-INF/view/list-customers.jsp
@@ -29,6 +29,14 @@
 		<div id = "container">
 			
 			<div id = "content">
+			
+			<!-- put new button: Add Customer -->
+			
+			<input type = "button" 
+				value = "Add Customer" 
+				onClick = "window.location.href='showFormForAdd'; return false;" 
+				class = "add-button"
+			/>
 				
 				<!-- add our html table here  -->
 				
--- web-customer-tracker/src/com/practice/springdemo/controller/CustomerController.java
+++ web-customer-tracker/src/com/practice/springdemo/controller/CustomerController.java
@@ -34,5 +34,18 @@ public class CustomerController {
 		
 	}
 	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+			
+		
+		// create model attribute to bind form data
+		Customer theCustomer = new Customer();
+		
+		theModel.addAttribute("customer", theCustomer);
+		
+		return "customer-form";
+			
+	}
+	
 
 }
