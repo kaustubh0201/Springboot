b65e3e5adea2a1316ce4db79a4debfb4ba6f1e1b
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-10-14 19:36:03 +0530

    added customer entity and dao

---
+++ web-customer-tracker/src/com/practice/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.practice.springdemo.dao;
+
+import java.util.List;
+
+import com.practice.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomer();
+	
+}
