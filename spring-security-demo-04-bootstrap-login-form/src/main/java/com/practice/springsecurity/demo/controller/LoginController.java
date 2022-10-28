60698f77e2c5f1bb1b7894b0609ceee4046a5db2
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-10-28 17:48:39 +0530

    bootstrap css login form

---
+++ spring-security-demo-04-bootstrap-login-form/src/main/java/com/practice/springsecurity/demo/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.practice.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
+		
+		return "plain-login";
+	}
+	
+}
