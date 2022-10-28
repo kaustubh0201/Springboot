1be784e299a94cdf061a5ace7401f29b1659a742
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-10-28 17:38:40 +0530

    added error showing in login page

--- spring-security-demo-03-custom-login-form/src/main/java/com/practice/springsecurity/demo/controller/LoginController.java
+++ spring-security-demo-03-custom-login-form/src/main/java/com/practice/springsecurity/demo/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.practice.springsecurity.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class LoginController {
 	
