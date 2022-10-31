14bc9c07b8ec201fc920a1f473a5be3acf40e06e
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-10-31 19:41:03 +0530

    JSON Jackson Demo - Nested Object and Arrays

---
+++ jackson-databind-json-demo/src/main/java/com/practice/jackson/json/demo/Driver.java
@@ -0,0 +1,30 @@
+package com.practice.jackson.json.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		
+		try {
+			
+			// create object mapper
+			ObjectMapper mapper = new ObjectMapper();
+			
+			// read JSON file and map/convert to Java POJO: data/sample-lite.json
+			
+			Student theStudent = mapper.readValue(new File("data/sample-full.json"), Student.class);
+			
+			// print first name and last name
+			System.out.println("First Name = " + theStudent.getFirstName());
+			System.out.println("Last Name = " + theStudent.getLastName());
+			
+		} 
+		catch(Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+
+}
