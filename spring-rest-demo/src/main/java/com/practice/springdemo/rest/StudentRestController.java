54102935675a74f23d71f264df25c89d77fcaef9
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-11-01 18:56:38 +0530

    Exception Handling - Spring REST

--- spring-rest-demo/src/main/java/com/practice/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/practice/springdemo/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,7 +37,7 @@ public class StudentRestController {
 	// define endpoint for "/students" - return list of students
 	
 	@GetMapping("/students")
-	public List<Student> getStudents() {		
+	public List<Student> getStudents() {
 		
 		return theStudents;
 		
@@ -47,7 +50,34 @@ public class StudentRestController {
 		
 		// just index into the list ... keep it simple for now
 		
+		// check the studentId against list size
+		
+		if( (studentId >= theStudents.size()) || (studentId < 0)) {
+			
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+			
+		}
+		
 		return theStudents.get(studentId);
 	}
 	
+	// add an exception handler using @ExceptionHandler
+	
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+		
+		// create a StudentErrorResponse
+		
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		// return ResponseEntity
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+		
+	}
+		
 }
