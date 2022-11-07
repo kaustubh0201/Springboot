49cdc4d05761b98cd6b749145639c1816eb432cd
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-11-07 20:01:14 +0530

    initializing springboot project

---
+++ mywebapp/src/main/java/com/practice/springboot/demo/mywebapp/MywebappApplication.java
@@ -0,0 +1,13 @@
+package com.practice.springboot.demo.mywebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MywebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MywebappApplication.class, args);
+	}
+
+}
