8a96432fdb08d3989f910ed2309db647855fc977
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-11-08 22:22:06 +0530

    spring boot crud app init

---
+++ 05-properties-demo/src/test/java/com/practice/springboot/demo/mywebapp/MywebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springboot.demo.mywebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MywebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
