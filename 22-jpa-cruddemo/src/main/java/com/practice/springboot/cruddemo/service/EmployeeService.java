f86b26d875cec6fb7e932bb29e10bc6c44f74284
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-11-09 22:12:34 +0530

    spring rest api using jpa

---
+++ 22-jpa-cruddemo/src/main/java/com/practice/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.practice.springboot.cruddemo.service;
+
+import java.util.List;
+
+import com.practice.springboot.cruddemo.entity.Employee;
+
+public interface EmployeeService {
+	
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+
+}
