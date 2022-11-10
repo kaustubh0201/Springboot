1c7e06d7f5061baa61eec862140fab3443848581
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-11-10 18:40:41 +0530

    spring data jpa repository

---
+++ 23-spring-data-jpa-cruddemo/src/main/java/com/practice/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,58 @@
+package com.practice.springboot.cruddemo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.practice.springboot.cruddemo.dao.EmployeeRepository;
+import com.practice.springboot.cruddemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+	
+	private EmployeeRepository employeeRepository;
+	
+	@Autowired
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
+
+	@Override
+	public List<Employee> findAll() {
+		
+		return employeeRepository.findAll();
+	}
+
+	@Override
+	public Employee findById(int theId) {
+		
+		Optional<Employee> result = employeeRepository.findById(theId);
+		
+		Employee theEmployee = null;
+		
+		if(result.isPresent()) {
+			theEmployee = result.get();
+		} else {
+			// we didn't find the employee
+			
+			throw new RuntimeException("Did not find employee id - " + theId);
+		}
+		
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+		employeeRepository.save(theEmployee);
+		
+	}
+
+	@Override
+	public void deleteById(int theId) {
+		employeeRepository.deleteById(theId);
+		
+	}
+	
+}
