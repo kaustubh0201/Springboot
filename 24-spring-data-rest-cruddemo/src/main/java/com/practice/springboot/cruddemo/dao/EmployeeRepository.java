f67ca9fd433af9b6f090cf1e9ab447df84795ce0
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-11-11 20:09:52 +0530

    adding thymeleaf templating engine

--- 24-spring-data-rest-cruddemo/src/main/java/com/practice/springboot/cruddemo/dao/EmployeeRepository.java
+++ 24-spring-data-rest-cruddemo/src/main/java/com/practice/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.practice.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.practice.springboot.cruddemo.entity.Employee;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
 	// that's it ... no need to write any other code
