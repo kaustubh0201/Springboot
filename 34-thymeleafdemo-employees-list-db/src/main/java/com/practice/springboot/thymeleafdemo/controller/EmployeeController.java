b95be490c67a5f62b903cce2b42d393db3575f2d
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-11-11 21:32:43 +0530

    initial of thymeleaf crud

---
+++ 34-thymeleafdemo-employees-list-db/src/main/java/com/practice/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,37 @@
+package com.practice.springboot.thymeleafdemo.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.practice.springboot.thymeleafdemo.entity.Employee;
+import com.practice.springboot.thymeleafdemo.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+	
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
+	}
+		
+	// add mapping for "/list"
+	
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		
+		// get employees from db
+		List<Employee> theEmployees = employeeService.findAll();
+		
+		// add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+		
+		return "list-employees";
+	}
+
+}
---
+++ 34-thymeleafdemo-employees-list-db/src/main/java/com/practice/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.practice.springboot.thymeleafdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.practice.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	// that's it ... no need to write any other code
+	
+}
