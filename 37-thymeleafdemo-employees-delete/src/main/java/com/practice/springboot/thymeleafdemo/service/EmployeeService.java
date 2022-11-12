472d3d421c37a3a6354daa2c74473890935d797c
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-11-12 10:54:10 +0530

    delete employees thymeleaf

---
+++ 37-thymeleafdemo-employees-delete/src/main/java/com/practice/springboot/thymeleafdemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.practice.springboot.thymeleafdemo.service;
+
+import java.util.List;
+
+import com.practice.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeService {
+	
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+
+}
