115dc42395ee30e2d6c36eb39e85779eb1a4f3ac
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-11-13 01:47:58 +0530

    uploading files service

---
+++ uploading-files/src/main/java/com/practice/springboot/uploadingfiles/UploadingFilesApplication.java
@@ -0,0 +1,28 @@
+package com.practice.springboot.uploadingfiles;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.practice.springboot.uploadingfiles.storage.StorageProperties;
+import com.practice.springboot.uploadingfiles.storage.StorageService;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}	
+
+}
